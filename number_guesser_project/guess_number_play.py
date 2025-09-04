4e2027b126bad2d01ee0e21ee809946f8f4e1adf
M. Mehdi Yazdanian <mehdi.yazdanian@hotmail.com>
2025-09-04 15:56:45 -0400

    Revised the files and folder for Number Gusser Project

--- number_guesser_project/guess_number_play.py
+++
@@ -1,4 +0,0 @@
-from guess_num_utils import play_guessing_game
-
-play_guessing_game()
-
---
+++ number_guesser_project/src/guess_number_play.py
@@ -0,0 +1,4 @@
+from number_guesser_project.src.guess_num_utils import play_guessing_game
+
+play_guessing_game()
+
